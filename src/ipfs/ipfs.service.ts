b3b77b23e6c2272b723e7f10f8e962ad705e921f
Shreyas K <shreyas.2000@hotmail.com>
2023-05-15 16:49:14 +0530

    chore: check passed id
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/ipfs/ipfs.service.ts
+++ src/ipfs/ipfs.service.ts
@@ -117,6 +117,7 @@ export class IpfsService {
 
   async download(hash) {
     const res = await this.uploadModel.findOne({ hash }).exec();
+    console.log(hash);
     if (res !== null)
       throw new BadRequestException('File not found with the given hash.');
 
